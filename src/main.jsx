5ee88dbc2ceac55ef3a7f0cc4bb4d76a899d4566
SamWilk <samwilk18@yahoo.com>
2024-09-03 18:27:44 -0400

    Removing import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Test from "./Test.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
